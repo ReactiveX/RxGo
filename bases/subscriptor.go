f0ca0bf4deb9ddd9ff92cc5e94b642725440c5e4
Joe Chasinga <jo.chasinga@gmail.com>
2016-12-28 14:25:35 -0500

    Edit Dispose to return nothing and add subscribeAt/unsubscribeAt getters

--- bases/subscriptor.go
+++ bases/subscriptor.go
@@ -4,9 +4,11 @@ import "time"
 
 // Subscriptor is an interface type which Subscription implements.
 type Subscriptor interface {
-	Dispose() Subscriptor
+	Dispose()
 	Subscribe() Subscriptor
+	SubscribeAt() time.Time
 	Unsubscribe() Subscriptor
+	UnsubscribeAt() time.Time
 	UnsubscribeIn(time.Duration) <-chan Subscriptor
 	UnsubscribeOn(chan struct{}, time.Duration) <-chan Subscriptor
 }
