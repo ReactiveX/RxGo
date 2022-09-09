db15c72e630379a286794c7d9f20348136c1a650
si3nloong <sianloong90@gmail.com>
2022-09-09 23:50:34 +0800

    refactor: filter and skip operation

--- filter.go
+++ filter.go
@@ -1,6 +1,9 @@
 package rxgo
 
-import "reflect"
+import (
+	"reflect"
+	"sync"
+)
 
 // Returns an Observable that emits all items emitted by the source Observable
 // that are distinct by comparison from previous items.
@@ -64,6 +67,44 @@ func DistinctUntilChanged[T any](comparator ...ComparatorFunc[T, T]) OperatorFun
 	}
 }
 
+// Emits the single value at the specified index in a sequence of emissions from the source Observable.
+func ElementAt[T any](pos uint, defaultValue ...T) OperatorFunc[T, T] {
+	return func(source Observable[T]) Observable[T] {
+		var (
+			index    uint
+			notEmpty bool
+		)
+		return createOperatorFunc(
+			source,
+			func(obs Observer[T], v T) {
+				if index == pos {
+					obs.Next(v)
+					obs.Complete()
+					notEmpty = true
+					return
+				}
+				index++
+			},
+			func(obs Observer[T], err error) {
+				obs.Error(err)
+			},
+			func(obs Observer[T]) {
+				if notEmpty {
+					return
+				}
+
+				if len(defaultValue) > 0 {
+					obs.Next(defaultValue[0])
+					obs.Complete()
+					return
+				}
+
+				obs.Error(ErrArgumentOutOfRange)
+			},
+		)
+	}
+}
+
 // Filter emits only those items from an Observable that pass a predicate test.
 func Filter[T any](predicate PredicateFunc[T]) OperatorFunc[T, T] {
 	return func(source Observable[T]) Observable[T] {
@@ -80,6 +121,397 @@ func Filter[T any](predicate PredicateFunc[T]) OperatorFunc[T, T] {
 				if cb(v, index) {
 					obs.Next(v)
 				}
+				index++
+			},
+			func(obs Observer[T], err error) {
+				obs.Error(err)
+			},
+			func(obs Observer[T]) {
+				obs.Complete()
+			},
+		)
+	}
+}
+
+// Emits only the first value (or the first value that meets some condition)
+// emitted by the source Observable.
+func First[T any](predicate PredicateFunc[T], defaultValue ...T) OperatorFunc[T, T] {
+	return func(source Observable[T]) Observable[T] {
+		var (
+			index    uint
+			hasValue bool
+		)
+		cb := skipPredicate[T]
+		if predicate != nil {
+			cb = predicate
+		}
+		return createOperatorFunc(
+			source,
+			func(obs Observer[T], v T) {
+				if !hasValue && cb(v, index) {
+					hasValue = true
+					obs.Next(v)
+					obs.Complete()
+					return
+				}
+				index++
+			},
+			func(obs Observer[T], err error) {
+				obs.Error(err)
+			},
+			func(obs Observer[T]) {
+				if !hasValue {
+					if len(defaultValue) == 0 {
+						obs.Error(ErrEmpty)
+						return
+					}
+
+					obs.Next(defaultValue[0])
+				}
+				obs.Complete()
+			},
+		)
+	}
+}
+
+// Returns an Observable that emits only the last item emitted by the source Observable.
+// It optionally takes a predicate function as a parameter, in which case,
+// rather than emitting the last item from the source Observable,
+// the resulting Observable will emit the last item from the source Observable
+// that satisfies the predicate.
+func Last[T any](predicate PredicateFunc[T], defaultValue ...T) OperatorFunc[T, T] {
+	return func(source Observable[T]) Observable[T] {
+		var (
+			index       uint
+			hasValue    bool
+			latestValue T
+			found       bool
+		)
+		cb := skipPredicate[T]
+		if predicate != nil {
+			cb = predicate
+		}
+		return createOperatorFunc(
+			source,
+			func(obs Observer[T], v T) {
+				hasValue = true
+				if cb(v, index) {
+					found = true
+					latestValue = v
+				}
+				index++
+			},
+			func(obs Observer[T], err error) {
+				obs.Error(err)
+			},
+			func(obs Observer[T]) {
+				if found {
+					obs.Next(latestValue)
+				} else {
+					if !hasValue {
+						if len(defaultValue) == 0 {
+							obs.Error(ErrEmpty)
+							return
+						}
+
+						obs.Next(defaultValue[0])
+					} else {
+						obs.Error(ErrNotFound)
+						return
+					}
+				}
+				obs.Complete()
+			},
+		)
+	}
+}
+
+// Ignores all items emitted by the source Observable and only passes calls of complete or error.
+func IgnoreElements[T any]() OperatorFunc[T, T] {
+	return func(source Observable[T]) Observable[T] {
+		return createOperatorFunc(
+			source,
+			func(obs Observer[T], v T) {},
+			func(obs Observer[T], err error) {
+				obs.Error(err)
+			},
+			func(obs Observer[T]) {
+				obs.Complete()
+			},
+		)
+	}
+}
+
+// Returns an Observable that skips the first count items emitted by the source Observable.
+func Skip[T any](count uint) OperatorFunc[T, T] {
+	return func(source Observable[T]) Observable[T] {
+		var (
+			index uint
+		)
+		return createOperatorFunc(
+			source,
+			func(obs Observer[T], v T) {
+				index++
+				if count >= index {
+					return
+				}
+				obs.Next(v)
+			},
+			func(obs Observer[T], err error) {
+				obs.Error(err)
+			},
+			func(obs Observer[T]) {
+				obs.Complete()
+			},
+		)
+	}
+}
+
+// Skip a specified number of values before the completion of an observable.
+func SkipLast[T any](skipCount uint) OperatorFunc[T, T] {
+	return func(source Observable[T]) Observable[T] {
+		var (
+			values = make([]T, 0)
+		)
+		return createOperatorFunc(
+			source,
+			func(obs Observer[T], v T) {
+				values = append(values, v)
+			},
+			func(obs Observer[T], err error) {
+				obs.Error(err)
+			},
+			func(obs Observer[T]) {
+				values = values[:uint(len(values))-skipCount]
+				for _, v := range values {
+					obs.Next(v)
+				}
+				obs.Complete()
+			},
+		)
+	}
+}
+
+// Returns an Observable that skips items emitted by the source Observable until a
+// second Observable emits an item.
+func SkipUntil[T any, R any](notifier Observable[R]) OperatorFunc[T, T] {
+	return func(source Observable[T]) Observable[T] {
+		return newObservable(func(subscriber Subscriber[T]) {
+			var (
+				wg = new(sync.WaitGroup)
+			)
+
+			wg.Add(2)
+
+			var (
+				skip         = true
+				upStream     = source.SubscribeOn(wg.Done)
+				notifyStream = notifier.SubscribeOn(wg.Done)
+			)
+
+			// It will never let the source observable emit any values if the
+			// notifier completes or throws an error without emitting a value before.
+
+		loop:
+			for {
+				select {
+				case <-subscriber.Closed():
+					upStream.Stop()
+					notifyStream.Stop()
+					break loop
+
+				case item, ok := <-upStream.ForEach():
+					if !ok {
+						notifyStream.Stop()
+						break loop
+					}
+
+					ended := item.Err() != nil || item.Done()
+					if ended {
+						notifyStream.Stop()
+						item.Send(subscriber)
+						break loop
+					}
+
+					if !skip {
+						item.Send(subscriber)
+					}
+
+				// Internally the skipUntil operator subscribes to the passed in observable
+				// (in the following called notifier) in order to recognize the emission of
+				// its first value. When this happens, the operator unsubscribes from the
+				// notifier and starts emitting the values of the source observable.
+				case <-notifyStream.ForEach():
+					notifyStream.Stop()
+					skip = false
+				}
+			}
+
+			wg.Wait()
+		})
+	}
+}
+
+// Returns an Observable that skips all items emitted by the source Observable
+// as long as a specified condition holds true, but emits all further source items
+// as soon as the condition becomes false.
+func SkipWhile[T any](predicate func(v T, index uint) bool) OperatorFunc[T, T] {
+	return func(source Observable[T]) Observable[T] {
+		var (
+			index uint
+			pass  bool
+		)
+		return createOperatorFunc(
+			source,
+			func(obs Observer[T], v T) {
+				if pass {
+					obs.Next(v)
+					return
+				}
+				if !predicate(v, index) {
+					pass = true
+					obs.Next(v)
+				}
+				index++
+			},
+			func(obs Observer[T], err error) {
+				obs.Error(err)
+			},
+			func(obs Observer[T]) {
+				obs.Complete()
+			},
+		)
+	}
+}
+
+// Emits only the first count values emitted by the source Observable.
+func Take[T any](count uint) OperatorFunc[T, T] {
+	if count == 0 {
+		return func(source Observable[T]) Observable[T] {
+			return EMPTY[T]()
+		}
+	}
+
+	return func(source Observable[T]) Observable[T] {
+		var (
+			seen = uint(0)
+		)
+		return createOperatorFunc(
+			source,
+			func(obs Observer[T], v T) {
+				seen++
+				if seen <= count {
+					obs.Next(v)
+					if count <= seen {
+						obs.Complete()
+					}
+				}
+			},
+			func(obs Observer[T], err error) {
+				obs.Error(err)
+			},
+			func(obs Observer[T]) {
+				obs.Complete()
+			},
+		)
+	}
+}
+
+// Waits for the source to complete, then emits the last N values from the source,
+// as specified by the count argument.
+func TakeLast[T any](count uint) OperatorFunc[T, T] {
+	return func(source Observable[T]) Observable[T] {
+		var (
+			values = make([]T, count)
+		)
+		return createOperatorFunc(
+			source,
+			func(obs Observer[T], v T) {
+				if uint(len(values)) >= count {
+					// shift the item from queue
+					values = values[1:]
+				}
+				values = append(values, v)
+			},
+			func(obs Observer[T], err error) {
+				obs.Error(err)
+			},
+			func(obs Observer[T]) {
+				for _, v := range values {
+					obs.Next(v)
+				}
+				obs.Complete()
+			},
+		)
+	}
+}
+
+// Emits the values emitted by the source Observable until a notifier Observable emits a value.
+func TakeUntil[T any, R any](notifier Observable[R]) OperatorFunc[T, T] {
+	return func(source Observable[T]) Observable[T] {
+		return newObservable(func(subscriber Subscriber[T]) {
+			var (
+				wg = new(sync.WaitGroup)
+			)
+
+			wg.Add(2)
+
+			var (
+				upStream     = source.SubscribeOn(wg.Done)
+				notifyStream = notifier.SubscribeOn(wg.Done)
+			)
+
+		loop:
+			for {
+				select {
+				case <-subscriber.Closed():
+					upStream.Stop()
+					notifyStream.Stop()
+					break loop
+
+				case item, ok := <-upStream.ForEach():
+					if !ok {
+						notifyStream.Stop()
+						break loop
+					}
+
+					ended := item.Err() != nil || item.Done()
+					item.Send(subscriber)
+					if ended {
+						notifyStream.Stop()
+						break loop
+					}
+
+				// Lets values pass until notifier Observable emits a value.
+				// Then, it completes.
+				case <-notifyStream.ForEach():
+					upStream.Stop()
+					notifyStream.Stop()
+					Complete[T]().Send(subscriber)
+					break loop
+				}
+			}
+
+			wg.Wait()
+		})
+	}
+}
+
+// Emits values emitted by the source Observable so long as each value satisfies the given predicate,
+// and then completes as soon as this predicate is not satisfied.
+func TakeWhile[T any](predicate func(value T, index uint) bool) OperatorFunc[T, T] {
+	return func(source Observable[T]) Observable[T] {
+		var (
+			index uint
+		)
+		return createOperatorFunc(
+			source,
+			func(obs Observer[T], v T) {
+				if !predicate(v, index) {
+					obs.Complete()
+					return
+				}
+				obs.Next(v)
 				index++
 			},
 			func(obs Observer[T], err error) {
--- filter_test.go
+++ filter_test.go
@@ -1,6 +1,10 @@
 package rxgo
 
-import "testing"
+import (
+	"errors"
+	"testing"
+	"time"
+)
 
 func TestDistinct(t *testing.T) {
 	t.Run("Distinct with EMPTY", func(t *testing.T) {
@@ -105,6 +109,214 @@ func TestDistinctUntilChanged(t *testing.T) {
 	})
 }
 
-func TestFilter(t *testing.T) {
+func TestElementAt(t *testing.T) {
+	t.Run("ElementAt with default value", func(t *testing.T) {
+		checkObservableResult(t, Pipe1(EMPTY[any](), ElementAt[any](1, 10)), 10, nil, true)
+	})
 
+	t.Run("ElementAt position 2", func(t *testing.T) {
+		checkObservableResult(t, Pipe1(Range[uint](1, 100), ElementAt[uint](2)), 3, nil, true)
+	})
+
+	t.Run("ElementAt with error (ErrArgumentOutOfRange)", func(t *testing.T) {
+		checkObservableResult(t, Pipe1(Range[uint](1, 10), ElementAt[uint](100)), 0, ErrArgumentOutOfRange, false)
+	})
+}
+
+func TestFilter(t *testing.T) {
+	t.Run("Filter with EMPTY", func(t *testing.T) {
+		checkObservableResult(t, Pipe1(EMPTY[any](), Filter[any](nil)), nil, nil, true)
+	})
+
+	t.Run("Filter with error", func(t *testing.T) {
+		var err = errors.New("throw")
+		checkObservableResult(t, Pipe1(
+			ThrownError[any](func() error {
+				return err
+			}), Filter[any](nil)), nil, err, false)
+	})
+
+	t.Run("Filter with Range(1,100)", func(t *testing.T) {
+		checkObservableResults(t, Pipe1(
+			Range[uint8](1, 100),
+			Filter(func(value uint8, index uint) bool {
+				return value <= 10
+			}),
+		), []uint8{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, nil, true)
+	})
+
+	t.Run("Filter with alphaberts", func(t *testing.T) {
+		checkObservableResults(t, Pipe1(
+			Scheduled("a", "b", "cd", "kill", "p", "z", "animal"),
+			Filter(func(value string, index uint) bool {
+				return len(value) == 1
+			}),
+		), []string{"a", "b", "p", "z"}, nil, true)
+	})
+}
+
+func TestFirst(t *testing.T) {
+	t.Run("First with EMPTY", func(t *testing.T) {
+		checkObservableResult(t, Pipe1(EMPTY[any](), First[any](nil)), nil, ErrEmpty, false)
+	})
+
+	t.Run("First with default value", func(t *testing.T) {
+		checkObservableResult(t, Pipe1(EMPTY[any](), First[any](nil, "hello default value")), "hello default value", nil, true)
+	})
+
+	t.Run("First with value", func(t *testing.T) {
+		checkObservableResult(t, Pipe1(Range[uint8](88, 99), First(func(value uint8, index uint) bool {
+			return value > 0
+		})), uint8(88), nil, true)
+	})
+}
+
+func TestLast(t *testing.T) {
+	t.Run("Last with empty value", func(t *testing.T) {
+		checkObservableResult(t, Pipe1(EMPTY[any](), Last[any](nil)), nil, ErrEmpty, false)
+	})
+
+	t.Run("Last with default value", func(t *testing.T) {
+		checkObservableResult(t, Pipe1(EMPTY[any](), Last[any](nil, 88)), 88, nil, true)
+	})
+
+	t.Run("Last with value", func(t *testing.T) {
+		checkObservableResult(t, Pipe1(Range[uint8](1, 72), Last[uint8](nil)), uint8(72), nil, true)
+	})
+
+	t.Run("Last with value but not matched", func(t *testing.T) {
+		checkObservableResult(t, Pipe1(Range[uint8](1, 10), Last(func(value uint8, _ uint) bool {
+			return value > 10
+		})), uint8(0), ErrNotFound, false)
+	})
+}
+
+func TestIgnoreElements(t *testing.T) {
+	t.Run("IgnoreElements with EMPTY", func(t *testing.T) {
+		checkObservableResult(t, Pipe1(EMPTY[any](), IgnoreElements[any]()), nil, nil, true)
+	})
+
+	t.Run("IgnoreElements with ThrownError", func(t *testing.T) {
+		var err = errors.New("throw")
+		checkObservableResult(t, Pipe1(ThrownError[error](func() error {
+			return err
+		}), IgnoreElements[error]()), nil, err, false)
+	})
+
+	t.Run("IgnoreElements with Range(1,7)", func(t *testing.T) {
+		checkObservableResult(t, Pipe1(Range[uint](1, 7), IgnoreElements[uint]()), uint(0), nil, true)
+	})
+}
+
+func TestSkip(t *testing.T) {
+	t.Run("Skip with EMPTY", func(t *testing.T) {
+		checkObservableResults(t, Pipe1(EMPTY[uint](), Skip[uint](5)), []uint{}, nil, true)
+	})
+
+	t.Run("Skip with Range(1,10)", func(t *testing.T) {
+		checkObservableResults(t, Pipe1(Range[uint](1, 10), Skip[uint](5)),
+			[]uint{6, 7, 8, 9, 10}, nil, true)
+	})
+
+	t.Run("Skip with ThrownError", func(t *testing.T) {
+		var err = errors.New("stop")
+		checkObservableResults(t, Pipe1(ThrownError[uint](func() error {
+			return err
+		}), Skip[uint](5)), []uint{}, err, false)
+	})
+
+	// t.Run("Skip with Scheduled", func(t *testing.T) {
+	// 	checkObservableResults(t,
+	// 		Pipe1(Scheduled[any](1, 2, errors.New("stop")), Skip[any](2)),
+	// 		[]any{1, 2}, nil, true)
+	// })
+}
+
+func TestSkipLast(t *testing.T) {
+	checkObservableResults(t, Pipe1(Range[uint](1, 10), SkipLast[uint](5)), []uint{1, 2, 3, 4, 5}, nil, true)
+}
+
+func TestSkipUntil(t *testing.T) {
+	t.Run("SkipUntil with EMPTY", func(t *testing.T) {
+		checkObservableResults(t, Pipe1(
+			EMPTY[uint](),
+			SkipUntil[uint](Scheduled("a")),
+		), []uint{}, nil, true)
+	})
+
+	t.Run("SkipUntil with ThrownError", func(t *testing.T) {
+		var err = errors.New("failed")
+		checkObservableResults(t, Pipe1(
+			ThrownError[uint](func() error {
+				return err
+			}),
+			SkipUntil[uint](Scheduled("a")),
+		), []uint{}, err, false)
+	})
+
+	t.Run("SkipUntil with Range(1,10)", func(t *testing.T) {
+		checkObservableResults(t, Pipe1(
+			Range[uint](1, 10),
+			SkipUntil[uint](Interval(time.Millisecond*100)),
+		), []uint{}, nil, true)
+	})
+}
+
+func TestSkipWhile(t *testing.T) {
+	t.Run("SkipWhile until condition meet", func(t *testing.T) {
+		checkObservableResults(t, Pipe1(
+			Scheduled("Green Arrow", "SuperMan", "Flash", "SuperGirl", "Black Canary"),
+			SkipWhile(func(v string, _ uint) bool {
+				return v != "SuperGirl"
+			})), []string{"SuperGirl", "Black Canary"}, nil, true)
+	})
+
+	t.Run("SkipWhile until index 5", func(t *testing.T) {
+		checkObservableResults(t, Pipe1(Range[uint](1, 10), SkipWhile(func(_ uint, idx uint) bool {
+			return idx != 5
+		})), []uint{6, 7, 8, 9, 10}, nil, true)
+	})
+}
+
+func TestTake(t *testing.T) {
+	checkObservableResults(t, Pipe1(Interval(time.Millisecond), Take[uint](3)), []uint{0, 1, 2}, nil, true)
+	checkObservableResults(t, Pipe1(Range[uint](1, 100), Take[uint](3)), []uint{1, 2, 3}, nil, true)
+}
+
+func TestTakeLast(t *testing.T) {
+	checkObservableResults(t, Pipe1(Range[uint](1, 100), TakeLast[uint](3)), []uint{98, 99, 100}, nil, true)
+}
+
+func TestTakeUntil(t *testing.T) {
+	t.Run("TakeUntil with EMPTY", func(t *testing.T) {
+		checkObservableResults(t, Pipe1(
+			EMPTY[uint](),
+			TakeUntil[uint](Scheduled("a")),
+		), []uint{}, nil, true)
+	})
+
+	t.Run("TakeUntil with Interval", func(t *testing.T) {
+		checkObservableResults(t, Pipe1(
+			Interval(time.Millisecond),
+			TakeUntil[uint](Interval(time.Millisecond*5)),
+		), []uint{0, 1, 2, 3}, nil, true)
+	})
+}
+
+func TestTakeWhile(t *testing.T) {
+	t.Run("TakeWhile with Interval", func(t *testing.T) {
+		result := make([]uint, 0)
+		for i := uint(0); i <= 5; i++ {
+			result = append(result, i)
+		}
+		checkObservableResults(t, Pipe1(Interval(time.Millisecond), TakeWhile(func(v uint, _ uint) bool {
+			return v <= 5
+		})), result, nil, true)
+	})
+
+	t.Run("TakeWhile with Range", func(t *testing.T) {
+		checkObservableResults(t, Pipe1(Range[uint](1, 100), TakeWhile(func(v uint, _ uint) bool {
+			return v >= 50
+		})), []uint{}, nil, true)
+	})
 }
