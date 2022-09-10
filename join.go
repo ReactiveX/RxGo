1c412a4752d59e2ecf3d9f7301cfdb00fe88f84a
si3nloong <sianloong90@gmail.com>
2022-09-10 12:00:34 +0800

    feat: introduce join operators, `ForkJoin` and `Zip`

---
+++ join.go
@@ -0,0 +1,199 @@
+package rxgo
+
+import (
+	"errors"
+	"sync"
+	"sync/atomic"
+)
+
+func CombineLatest() {
+
+}
+
+// Accepts an Array of ObservableInput or a dictionary Object of ObservableInput
+// and returns an Observable that emits either an array of values in the exact same
+// order as the passed array, or a dictionary of values in the same shape as the
+// passed dictionary.
+func ForkJoin[T any](sources ...Observable[T]) Observable[[]T] {
+	return newObservable(func(subscriber Subscriber[[]T]) {
+		var (
+			noOfSource = len(sources)
+		)
+		// forkJoin is an operator that takes any number of input observables which can be
+		// passed either as an array or a dictionary of input observables. If no input
+		// observables are provided (e.g. an empty array is passed), then the resulting
+		// stream will complete immediately.
+		if noOfSource < 1 {
+			Complete[[]T]().Send(subscriber)
+			return
+		}
+
+		var (
+			wg  = new(sync.WaitGroup)
+			mu  = new(sync.Mutex)
+			err = new(atomic.Pointer[error])
+			// Single buffered channel will not accept more than one signal
+			errCh         = make(chan error, 1)
+			stopCh        = make(chan struct{})
+			latestValues  = make([]T, noOfSource)
+			subscriptions = make([]Subscriber[T], noOfSource)
+		)
+
+		wg.Add(noOfSource)
+
+		go func() {
+			select {
+			case <-subscriber.Closed():
+				return
+			case v, ok := <-errCh:
+				if !ok {
+					return
+				}
+				err.Swap(&v)
+				close(stopCh)
+			}
+		}()
+
+		// In order for the resulting array to have the same length as the number of
+		// input observables, whenever any of the given observables completes without
+		// emitting any value, forkJoin will complete at that moment as well and it
+		// will not emit anything either, even if it already has some last values
+		// from other observables.
+		onNext := func(index int, v T) {
+			mu.Lock()
+			defer mu.Unlock()
+			latestValues[index] = v
+		}
+
+		observeStream := func(index int, upStream Subscriber[T]) {
+		observe:
+			for {
+				select {
+				case <-stopCh:
+					upStream.Stop()
+					break observe
+
+				case <-subscriber.Closed():
+					upStream.Stop()
+					break observe
+
+				case item, ok := <-upStream.ForEach():
+					if !ok {
+						break observe
+					}
+
+					// if one error, everything error
+					if err := item.Err(); err != nil {
+						errCh <- err
+						break observe
+					}
+
+					if item.Done() {
+						break observe
+					}
+
+					// forkJoin will wait for all passed observables to emit and complete
+					// and then it will emit an array or an object with last values from
+					// corresponding observables.
+					onNext(index, item.Value())
+				}
+			}
+		}
+
+		for i, source := range sources {
+			subscriber := source.SubscribeOn(wg.Done)
+			subscriptions[i] = subscriber
+			go observeStream(i, subscriber)
+		}
+
+		wg.Wait()
+
+		// Remove dangling go-routine
+		select {
+		case <-errCh:
+		default:
+			// Close error channel gracefully
+			close(errCh)
+		}
+
+		for _, sub := range subscriptions {
+			sub.Stop()
+		}
+
+		if exception := err.Load(); exception != nil {
+			if errors.Is(*exception, ErrEmpty) {
+				Complete[[]T]().Send(subscriber)
+				return
+			}
+
+			Error[[]T](*exception).Send(subscriber)
+			return
+		}
+
+		Next(latestValues).Send(subscriber)
+		Complete[[]T]().Send(subscriber)
+	})
+}
+
+// Combines multiple Observables to create an Observable whose values are calculated
+// from the values, in order, of each of its input Observables.
+func Zip[T any](sources ...Observable[T]) Observable[[]T] {
+	return newObservable(func(subscriber Subscriber[[]T]) {
+		var (
+			wg         = new(sync.WaitGroup)
+			noOfSource = len(sources)
+			observers  = make([]Subscriber[T], 0, noOfSource)
+		)
+
+		wg.Add(noOfSource)
+
+		for _, source := range sources {
+			observers = append(observers, source.SubscribeOn(wg.Done))
+		}
+
+		unsubscribeAll := func() {
+			for _, obs := range observers {
+				obs.Stop()
+			}
+		}
+
+		var (
+			result    = make([]T, noOfSource)
+			completes = [2]uint{} // true | false
+		)
+	loop:
+		for {
+			for i, obs := range observers {
+				select {
+				case <-subscriber.Closed():
+					unsubscribeAll()
+					break loop
+
+				case item, ok := <-obs.ForEach():
+					if !ok {
+						completes[1]++
+					} else if ok || item.Done() {
+						completes[0]++
+					}
+
+					if item != nil {
+						result[i] = item.Value()
+					}
+				}
+			}
+
+			if completes[1] >= uint(noOfSource) {
+				Complete[[]T]().Send(subscriber)
+				break loop
+			}
+
+			Next(result).Send(subscriber)
+
+			// Reset the values for next loop
+			result = make([]T, noOfSource)
+			completes = [2]uint{}
+		}
+
+		wg.Wait()
+	})
+}
---
+++ join_test.go
@@ -0,0 +1,92 @@
+package rxgo
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestForkJoin(t *testing.T) {
+	t.Run("ForkJoin with one EMPTY", func(t *testing.T) {
+		// ForkJoin only capture all latest value from every stream
+		checkObservableResult(t, ForkJoin(
+			EMPTY[any](),
+			Scheduled[any]("j", "k", "end"),
+			Pipe1(Range[uint](1, 10), Map(func(v, _ uint) (any, error) {
+				return v, nil
+			})),
+		), []any{nil, "end", uint(10)}, nil, true)
+	})
+
+	t.Run("ForkJoin with all EMPTY", func(t *testing.T) {
+		checkObservableResult(t, ForkJoin(
+			EMPTY[uint](),
+			EMPTY[uint](),
+			EMPTY[uint](),
+		), []uint{0, 0, 0}, nil, true)
+	})
+
+	t.Run("ForkJoin with error observable", func(t *testing.T) {
+		var err = fmt.Errorf("failed")
+		checkObservableResult(t, ForkJoin(
+			Scheduled[uint](1, 88, 2, 7215251),
+			Pipe1(Interval(time.Millisecond*10), Map(func(v, _ uint) (uint, error) {
+				return v, err
+			})),
+			Interval(time.Millisecond*100),
+		), nil, err, false)
+	})
+
+	t.Run("ForkJoin with multiple error", func(t *testing.T) {
+		createErr := func(index uint) error {
+			return fmt.Errorf("failed at %d", index)
+		}
+		checkObservableResultWithAnyError(t, ForkJoin(
+			ThrownError[string](func() error {
+				return createErr(1)
+			}),
+			ThrownError[string](func() error {
+				return createErr(2)
+			}),
+			ThrownError[string](func() error {
+				return createErr(3)
+			}),
+			Scheduled("a"),
+		), nil, []error{createErr(1), createErr(2), createErr(3)}, false)
+	})
+
+	t.Run("ForkJoin with complete", func(t *testing.T) {
+		checkObservableResult(t, ForkJoin(
+			Scheduled[uint](1, 88, 2, 7215251),
+			Pipe1(Interval(time.Millisecond*10), Take[uint](3)),
+		), []uint{7215251, 2}, nil, true)
+	})
+}
+
+func TestZip(t *testing.T) {
+	t.Run("Zip with Scheduled", func(t *testing.T) {
+		checkObservableResults(t, Zip(
+			EMPTY[any](),
+			Scheduled[any]("Foo", "Bar", "Beer"),
+			Scheduled[any](true, true, false),
+		), [][]any{
+			{nil, "Foo", true},
+			{nil, "Bar", true},
+			{nil, "Beer", false},
+			{nil, nil, nil},
+		}, nil, true)
+	})
+
+	t.Run("Zip with Scheduled", func(t *testing.T) {
+		checkObservableResults(t, Zip(
+			Scheduled[any](27, 25, 29),
+			Scheduled[any]("Foo", "Bar", "Beer"),
+			Scheduled[any](true, true, false),
+		), [][]any{
+			{27, "Foo", true},
+			{25, "Bar", true},
+			{29, "Beer", false},
+			{nil, nil, nil},
+		}, nil, true)
+	})
+}
